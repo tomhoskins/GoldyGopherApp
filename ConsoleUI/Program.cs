03c4cf649dd539ee2bd401ea2ee72d6452b8d817
th0712 <tomhoskins0712@gmail.com>
2025-02-08 15:19:33 -0600

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Welcome to the Goldy Gopher App by Tom Hoskins");
+Console.WriteLine();
+
+int lowerBound;
+int upperBound;
+
+while (true)
+{
+	Console.WriteLine("Enter the lower bound of the range of numbers to check:");
+	if (!int.TryParse(Console.ReadLine(), out lowerBound))
+	{
+        Console.WriteLine("Please enter a valid integer value.");
+        continue;
+    }
+
+    Console.WriteLine("Enter the upper bound of the range of numbers to check:");
+    if (!int.TryParse(Console.ReadLine(), out upperBound))
+    {
+        Console.WriteLine("Please enter a valid integer value.");
+        continue;
+    }
+
+    if (!(lowerBound < upperBound))
+    {
+        Console.WriteLine("The upper bound must be greater than the lower bound.");
+        continue;
+    }
+
+    break;
+}
+
+for (int i = lowerBound; i <= upperBound; i++)
+{
+    var result = GoldyGopherLibrary.Utilities.GetGopherStringFromInt(i);
+    Console.WriteLine($"{i}: {result}");
+}
---
+++ GoldyGopherLibrary/Utilities.cs
@@ -0,0 +1,39 @@
+﻿namespace GoldyGopherLibrary
+{
+    public static class Utilities
+    {
+        public static string GetGopherStringFromInt(int input)
+        {
+            // Using the following definition for multiple:
+            // B is a multiple of A if integer N exists such that B = A * N.
+
+            // If input divided by X has a remainder of 0, then input is a multiple of X.
+
+            // Per requirements:
+            // If input is a multiple of 3 and 7, return "Goldy Gopher".
+            // If input is a multiple of 3, return "Goldy".
+            // If input is a multiple of 7, return "Gopher".
+            // Otherwise, return the input as a string.
+
+            bool isGoldy = input % 3 == 0;
+            bool isGopher = input % 7 == 0;
+
+            if (isGoldy && isGopher)
+            {
+                return "Goldy Gopher";
+            }
+            else if (isGoldy)
+            {
+                return "Goldy";
+            }
+            else if (isGopher)
+            {
+                return "Gopher";
+            }
+            else
+            {
+                return input.ToString();
+            }
+        }
+    }
+}
